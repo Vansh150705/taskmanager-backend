2cb526c11049d5743abc0cc701a40cdc610d7792
Vansh150705 <vansh150705@gmail.com>
2025-08-05 23:20:32 +0530

    Mongoose Error Correction

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use('/api/messages', messageRoutes);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://vansh150705:Napv3955@taskmanager.dsqgvkh.mongodb.net/?retryWrites=true&w=majority&appName=TaskManager", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
